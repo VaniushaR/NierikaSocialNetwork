da0dc8fe2d79c015989049258cf92ba2274da4c0
VaniushaR <vaniusha.ra@gmail.com>
2018-12-19 18:14:08 -0600

    Updates

---
+++ precache-manifest.abdf4da49309fd02a0cdccac9f99c427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "907b717aa4349b467271",
+    "url": "/NierikaSocialNetwork/static/css/main.e2281b04.chunk.css"
+  },
+  {
+    "revision": "907b717aa4349b467271",
+    "url": "/NierikaSocialNetwork/static/js/main.907b717a.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/NierikaSocialNetwork/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "4da34cd29ec2081439a1",
+    "url": "/NierikaSocialNetwork/static/js/runtime~main.4da34cd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NierikaSocialNetwork/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4ca4fc8a431ed03ca289ac031881c0f",
+    "url": "/NierikaSocialNetwork/index.html"
+  }
+];
